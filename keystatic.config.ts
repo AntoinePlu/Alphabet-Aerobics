e277340220707658b780cb09ececeb51a1b0e25d
François Vaux <francois@yapok.org>
2023-07-18 09:38:02 +0200

    feat: Add basic Keystatic setup

---
+++ keystatic.config.ts
@@ -0,0 +1,17 @@
+// keystatic.config.ts
+import { config, fields, singleton } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+  singletons: {
+    homepage: singleton({
+      label: "Homepage",
+      path: "src/content/_homepage",
+      schema: {
+        headline: fields.text({ label: "Headline" }),
+      },
+    }),
+  },
+});
